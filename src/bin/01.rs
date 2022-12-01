380f3089183b4a33bb421c73b6239fcfff3c219e
Erik W. Bjønnes <erik@erikwb.com>
2022-12-01 21:48:09 +0100

    Updated examples/01.txt and corresponding values in tests. Now they work fine as well.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -79,12 +79,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24000u32));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(41000u32));
     }
 }
